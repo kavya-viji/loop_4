d9731c1cbc3bc9fc74052053cfac0264d6ca4483
Admin <kavyak6999@gmail.com>
2022-12-13 13:05:41 +0530

    else-if-grade

---
+++ loop/loop3.js
@@ -0,0 +1,21 @@
+var x = parseInt (prompt ("Enter the Percentage"));
+if(x>=90 && x<=100)
+{
+alert ("Grade O");
+}
+else if (x>=75 && x<=89)
+{
+alert ("Grade A");
+}
+else if (x>=60 && x<=74)
+{
+alert ("Grade B");
+}
+else if (x>=40 && x<=59)
+{
+alert ("Grade C");
+}
+else
+{
+alert ("Grade D");
+}
